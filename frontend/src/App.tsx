5929b97d40e083d0f5a7dacdcbbf74272537d073
nipunaupeksha <nipuna.upeksha@gmail.com>
2024-12-19 22:46:04 +0200

    setup: React App

---
+++ frontend/src/App.tsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count</button>
+        <p>{count}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
