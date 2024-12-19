feff5ceac5a61e88f42a0828cc9d596227e2d3db
nipunaupeksha <nipuna.upeksha@gmail.com>
2024-12-19 18:38:38 +0200

    add: zod validator

--- routes/expenses.ts
+++ routes/expenses.ts
@@ -1,4 +1,5 @@
 import { Hono } from "hono";
+import { z } from "zod";
 
 type Expense = {
   id: number;
@@ -14,14 +15,20 @@ const fakeExpenses: Expense[] = [
   { id: 5, title: "Gym Membership", amount: 35.0 },
 ];
 
+const createPostSchema = z.object({
+  title: z.string().min(3).max(100),
+  amount: z.number().int().positive(),
+});
+
 export const expensesRoute = new Hono()
   .get("/", (c) => {
     return c.json({ expenses: fakeExpenses });
   })
   .post("/", async (c) => {
-    const expense = await c.req.json();
+    const data = await c.req.json();
+    const expense = createPostSchema.parse(data);
     console.log(expense);
-    return c.json({expense});
+    return c.json({ expense });
   });
 // .delete()
 // .put()
