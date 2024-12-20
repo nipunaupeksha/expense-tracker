4936791a67c2424bab2ce429bace7ad1ca4f9990
nipunaupeksha <nipuna.upeksha@gmail.com>
2024-12-20 16:18:40 +0200

    add: kinde

--- server/app.ts
+++ server/app.ts
@@ -12,7 +12,10 @@ app.get("/test", (c) => {
   return c.json({ message: "test" });
 });
 
-const apiRoutes = app.basePath("/api").route("/expenses", expensesRoute);
+const apiRoutes = app
+  .basePath("/api")
+  .route("/expenses", expensesRoute)
+  .route("/", authRoute);
 
 app.get("*", serveStatic({ root: "./frontend/dist" }));
 app.get("*", serveStatic({ path: "./frontend/dist/index.html" }));
