5a11d8275e47b406166b2e0cfa88d8c205ce0c19
nipunaupeksha <nipuna.upeksha@gmail.com>
2024-12-20 15:49:06 +0200

    add: kinde

--- server/app.ts
+++ server/app.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { expensesRoute } from "./routes/expenses";
+import { authRoute } from "./routes/auth";
 import { serveStatic } from "hono/bun";
 
 const app = new Hono();
