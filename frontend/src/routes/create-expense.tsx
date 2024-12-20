1b1d41bef8a71f163b4186786307aabfccb822ac
nipunaupeksha <nipuna.upeksha@gmail.com>
2024-12-20 14:05:15 +0200

    add: basic form

--- frontend/src/routes/create-expense.tsx
+++ frontend/src/routes/create-expense.tsx
@@ -1,9 +1,23 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
 
-export const Route = createFileRoute('/create-expense')({
+export const Route = createFileRoute("/create-expense")({
   component: CreateExpenses,
-})
+});
 
 function CreateExpenses() {
-  return <div className="p-2">Hello /create-expense</div>
+  return (
+    <div className="p-2">
+      <h2>Create Expense</h2>
+      <form className="max-w-xl m-auto">
+        <Label htmlFor="title">Title</Label>
+        <Input type="text" id="title" placeholder="Title" />
+        <Label htmlFor="amount">Amount</Label>
+        <Input type="text" id="amount" placeholder="Amount" />
+        <Button type="submit">Create Expense</Button>
+      </form>
+    </div>
+  );
 }
