dea54a49d8aad777bc5bc58a4bf7d0e7fbc35292
nipunaupeksha <nipuna.upeksha@gmail.com>
2024-12-20 10:58:32 +0200

    add:server configs

--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,11 @@
+import { serialize } from "hono/utils/cookie";
 import app from "./app";
 
 const port = process.env.PORT || 8000;
 
-Bun.serve({
+const server = Bun.serve({
   port,
   fetch: app.fetch,
 });
 
-console.log("Server running");
+console.log("Server running", server.port);
